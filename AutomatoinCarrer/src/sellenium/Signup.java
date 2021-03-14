cde7a92d401f04d6be81029be3ebcecb77b6038b
LENOVO <LENOVO@DESKTOP-2LCEBJK>
2021-03-14 22:22:45 +0530

    intialcommit

---
+++ AutomatoinCarrer/src/sellenium/Signup.java
@@ -0,0 +1,25 @@
+package sellenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Signup {
+	static String san = "santhosh";
+	static int value = 10;
+	
+	public static void san()
+	
+	{
+		
+		System.out.println(san);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		san();
+		
+	}
+
+}
